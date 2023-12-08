088e2fd4c1d2fbf5d7458a2bb2269e159adb9957
Koromonnnnnnnn <764363@dpsk12.net>
2023-12-08 09:07:33 -0700

    f

--- Main/ColorWheel.cpp
+++ Main/ColorWheel.cpp
@@ -9,34 +9,68 @@ int main()
 
     const int testCases = gettestCases();
     string finalOutput = "";
-    string colors = {"Blue-Violet", "Blue", "Blue-Green", "Green", "Orange", "Red", "Red-Orange", "Red - Violet", "Violet", "Yellow", "Yellow-Green", "Yellow-Orange"};
+    string colors[] = {"blue-violet", "blue", "blue-green", "green", "orange", "red", "red-orange", "red-violet", "violet", "yellow", "yellow-green", "yellow-orange"};
 
     for (int testcase = 0; testcase < testCases; testcase++)
     {
         string color;
         getline(cin, color);
 
-        if (color == "violet")
+        if (color == colors[0])
         {
-            finalOutput += "In order to make violet, blue and red must be mixed.";
+            finalOutput += "In order to make blue-violet, blue and red must be mixed."; //blue violet
         }
-        else if (color == "blue-green")
+        else if (color == colors[1])
         {
-            finalOutput += "In order to make blue-green, blue and yellow must be mixed.";
+            finalOutput += "No colors need to be mixed to make blue."; // blue
         }
-        else if (color == "yellow")
+        else if (color == colors[2])
         {
-            finalOutput += "No colors need to be mixed to make yellow.";
+            finalOutput += "In order to make blue-green, blue and yellow must be mixed."; // blue green
         }
-        else if (color == "orange")
+        else if (color == colors[3])
         {
-            finalOutput += "In order to make orange, red and yellow must be mixed.";
+            finalOutput += "In order to make green, blue and yellow must be mixed."; //green
         }
+        else if (color == colors[4])
+        {
+            finalOutput += "In order to make orange, red and yellow must be mixed."; //orange
+        }
+        else if (color == colors[5])
+        {
+            finalOutput += "No colors need to be mixed to make red."; //red
+        }
+        else if (color == colors[6])
+        {
+            finalOutput += "In order to make red-orange, red and yellow must be mixed."; //red orange
+        }
+        else if (color == colors[7])
+        {
+            finalOutput += "In order to make red-violet, blue and red must be mixed."; //red violet
+        }
+        else if (color == colors[8])
+        {
+            finalOutput += "In order to make violet, blue and red must be mixed."; //violet
+        }
+        else if (color == colors[9])
+        {
+            finalOutput += "No colors need to be mixed to make yellow."; //yellow
+        }
+        else if (color == colors[10])
+        {
+            finalOutput += "In order to make yellow-green, yellow and blue must be mixed."; //yellow-green
+        }
+        else if (color == colors[11])
+        {
+            finalOutput += "In order to make yellow orange, yellow and red must be mixed."; //yelow orange
+        }
+
         if (testcase != (testCases - 1))
         {
             finalOutput += "\n";
         }
     }
+
     cout << finalOutput;
 
     return 0;
